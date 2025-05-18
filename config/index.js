2f1574af22dcbfe3bf54b38ac1eda30291a9023b
xuqian67 <1769197092@qq.com>
2025-05-18 10:27:07 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx9b923dae4a225e4f",
+  appsecret: "9bb2f8a64e9ec76a30bf849af5f3657b",
+  template_id: "srFvlvVAKDZHn11jXaY2maG1eW_5-oteSFaOJFCzYoI",
+  touser: "ohwa97WnF7Kqn5Fhs8wiT2Q57bOk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "南京",
+  first_date: "2025-5-14",
 };
